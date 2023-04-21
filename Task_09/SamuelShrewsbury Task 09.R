e83a8d26c15dca732c559f1e01e2d2897052be40
SamuelShrewsbury <sds0052@mix.wvu.edu>
2023-04-21 13:24:14 -0400

    SamuelShrewsbury Task 09

--- Task_09/SamuelShrewsbury Task 09.R
+++ Task_09/SamuelShrewsbury Task 09.R
@@ -14,7 +14,37 @@ str(simTree)
 ?tree.bisse
 stateTable<-table(simTree$tip.state)
 stateTable/sum(stateTable)
-#My state 0 is 0.49 while my state 1 is 0.51. This suggests that the that trait will slowly evolve instead of going extinct.
-#Other than just rates, the ability to inherit or learn from parents is important for a trait being passed on and present.
-#When state 1 speciation rate increases, it is more likely that the trait will be passed on to other generations or species. 
-#Net diversification is of state 1 increases when speciation rate is high.
+speciation_1range<-seq(0.1,1.0,0.1)
+sim_results<-list()
+for(i in seq_along(speciation_1range)){
+  Pars[2]<-speciation_1range[i]
+  simTree<-tree.bisse(Pars,max.taxa=maxN,max.t=maxT)
+                      sim_results[[i]]<-simTree
+}
+state_tables<-lapply(sim_results,function(tree)table(tree$tip.state)/
+                      sum(tree$tip.state))
+state_tables
+df<-data.frame(speciation_1=speciation_1range,tip_state_0=rep(0,
+              length(speciation_1range)),tip_state_1=rep(0,
+              length(speciation_1range)))
+for(i in seq_along(sim_results)){
+  state_table<-state_tables[[i]]
+  df$tip_state_0[i]<-state_table[1]
+  df$tip_state_1[i]<-state_table[2]
+}
+nona_df<-na.omit(df)
+nona_df
+library(ggplot2)
+ggplot(nona_df,aes(x=speciation_1))+
+  geom_line(aes(y=tip_state_0,color="Tip State 0"))+
+  geom_line(aes(y=tip_state_1,color="Tip State 1"))+
+  scale_color_manual(values=c("Tip State 0"="red","Tip State 1"=
+                                "black"))+
+  xlab("Speciation Rate for State 1")+
+  ylab("Tip State Frequency")+
+  ggtitle("Speciation vs Tip State Frequency")+
+  theme_bw()+
+  labs(color="Tip States")
+nona_df
+#The tip state frequency decreases as the speciation rate increases going closer to the 1 maximum. 
+#The tip state 1 frequency stays at one no matter what the speciation rate is. 
